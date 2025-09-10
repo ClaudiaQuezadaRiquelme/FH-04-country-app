8337a13f111ec8ee5a4f06533cb6a95ebb742ba2
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-10 16:46:41 -0300

    country pages with components

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CountrySearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
+  imports: [
+    CountrySearchInputComponent,
+    CountryListComponent,
+  ],
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  search = signal<string>('');
 
+  onSearch(value: string) {
+    console.log('search value:', value);
+
+  }
 }
