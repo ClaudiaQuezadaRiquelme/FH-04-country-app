17850640bfe796e377ee0456358e53f9d59705f8
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-10 15:39:21 -0300

    country nav with routes and components

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 
 export const countryRoutes: Routes = [
   {
@@ -10,6 +12,14 @@ export const countryRoutes: Routes = [
       {
         path: 'by-capital',
         component: ByCapitalPageComponent,
+      },
+      {
+        path: 'by-country',
+        component: ByCountryPageComponent,
+      },
+      {
+        path: 'by-region',
+        component: ByRegionPageComponent,
       },
       {
         path: '**',
