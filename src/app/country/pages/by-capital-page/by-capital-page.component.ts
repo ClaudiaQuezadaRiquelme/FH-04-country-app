44f4da14b96e9d723f4732a6db2f6da4094c7446
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-11 10:28:20 -0300

    show countries restapi response

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { CountrySearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
+import { RESTCountry } from '../../interfaces/rest-countries.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -14,10 +15,20 @@ import { CountryService } from '../../services/country.service';
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
 
+  isLoading = signal(false);
+  isError = signal<string|null>(null);
+  countries = signal<RESTCountry[]>([]);
+
   onSearch(query: string) {
-    console.log('search value:', query);
-    const res = this.countryService.searchByCapital(query);
-    res.subscribe(res => {
+    if (this.isLoading()) return; // evitar hacer peticiones a la api innecesarias.
+
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchByCapital(query)
+    .subscribe(res => {
+      this.isLoading.set(false);
+      this.countries.set(res);
       console.log(res);
     });
 
