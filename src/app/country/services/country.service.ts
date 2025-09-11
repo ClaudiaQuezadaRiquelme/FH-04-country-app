2e1ac7c7b5a11817cd15a7aa50b3b1cd382fa1e6
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-11 15:33:23 -0300

    loading and errors in country-list

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -34,6 +34,7 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(url)
       .pipe(
         map( res => CountryMapper.mapRESTCountryArrayToCountryArray(res) ),
+        delay(1500), // solo se usa para ver el comportamiento del loading
         catchError( err => {
           console.log('Error fetching: ', err);
           return throwError( ()=> new Error('No se pudo obtener países con esa query'));
