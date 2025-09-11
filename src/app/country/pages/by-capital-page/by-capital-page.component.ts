e5713b8a8b81952233cde93a289522b2ea612190
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-11 09:47:06 -0300

    countries rest api

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CountrySearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,10 +12,15 @@ import { CountryListComponent } from '../../components/country-list/country-list
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
-  search = signal<string>('');
+  countryService = inject(CountryService);
 
-  onSearch(value: string) {
-    console.log('search value:', value);
+  onSearch(query: string) {
+    console.log('search value:', query);
+    const res = this.countryService.searchByCapital(query);
+    res.subscribe(res => {
+      console.log(res);
+    });
+
 
   }
 }
